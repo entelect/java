b655a81158c42ebc4da3003261cd01f68db3dcff
ronald22 <ronaldm2@discovery.co.za>
2017-12-14 14:53:01 +0200

    refactor

--- entelect-tools/spring-jpa-maven-plugin/src/main/java/za/co/entelect/tools/maven/SpringJpaMojo.java
+++ entelect-tools/spring-jpa-maven-plugin/src/main/java/za/co/entelect/tools/maven/SpringJpaMojo.java
@@ -2,9 +2,6 @@ package za.co.entelect.tools.maven;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.maven.artifact.Artifact;
-import org.apache.maven.artifact.DefaultArtifact;
-import org.apache.maven.artifact.handler.DefaultArtifactHandler;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
@@ -13,13 +10,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.codehaus.plexus.classworlds.realm.ClassRealm;
-import org.eclipse.aether.collection.CollectRequest;
-import org.eclipse.aether.graph.Dependency;
-import org.eclipse.aether.graph.DependencyFilter;
-import org.eclipse.aether.resolution.DependencyRequest;
 import org.eclipse.aether.spi.locator.ServiceLocator;
-import org.eclipse.aether.util.filter.DependencyFilterUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
@@ -53,7 +44,6 @@ public abstract class SpringJpaMojo extends AbstractMojo {
     @Component
     protected BuildPluginManager pluginManager;
 
-    //@Component
     ServiceLocator serviceLocator;
 
     @Override
@@ -77,35 +67,6 @@ public abstract class SpringJpaMojo extends AbstractMojo {
 
         doExecute();
     }
-
-    private DependencyRequest dependencyRequest(org.eclipse.aether.artifact.DefaultArtifact artifact, String scope) {
-        final DependencyFilter filter =
-                DependencyFilterUtils.classpathFilter(scope);
-        CollectRequest request = new CollectRequest();
-        request.setRoot(new Dependency(artifact, scope));
-        request.setRepositories(mavenProject.getRemoteProjectRepositories());
-
-        return new DependencyRequest(request, filter);
-    }
-
-    private void configureClassLoader() throws MojoExecutionException {
-        try {
-            DefaultArtifactHandler artifactHandler = new DefaultArtifactHandler("jar");
-            DefaultArtifact artifact = new DefaultArtifact("org.eclipse.aether", "aether-spi", "0.9.1.v20140329", "compile", "jar", "", artifactHandler);
-            Artifact resolvedArtifact = mavenSession.getLocalRepository().find(artifact);
-
-            ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
-            if (contextClassLoader instanceof ClassRealm) {
-                ClassRealm classRealm = (ClassRealm) contextClassLoader;
-                classRealm.addURL(resolvedArtifact.getFile().toURI().toURL());
-            }
-        } catch (Exception e) {
-            PluginLogger.error(e.getMessage());
-            e.printStackTrace();
-            throw new MojoExecutionException("Failed to create parent-last child-first class loader", e);
-        }
-    }
-
 
     protected void doExecute() throws MojoExecutionException, MojoFailureException {
     }
